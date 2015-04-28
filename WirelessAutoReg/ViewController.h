edd541aab7e18aea6b1e8ad9afdc20424ef481f1
xuan chen <xuanchen@xuantekiiMac.local>
2015-04-29 07:59:52 +0800

    Initial Commit

---
+++ WirelessAutoReg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WirelessAutoReg
+//
+//  Created by xuan chen on 15/4/29.
+//  Copyright (c) 2015年 xuan chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
